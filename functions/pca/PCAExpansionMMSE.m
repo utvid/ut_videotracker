319dad6ba84fd3d99894f3a288adfffe4cdc2fa2
merijnes <merijnes@gmail.com>
2014-05-23 11:20:24 +0200

    Xest _or

--- functions/pca/PCAExpansionMMSE.m
+++ functions/pca/PCAExpansionMMSE.m
@@ -3,15 +3,16 @@ function [PCAcoords,Xest,Kal] = PCAExpansionMMSE(FrameL,FrameR,FrameM,n,PCAcoord
 imnL = FrameL; imnR = FrameR; imnM = FrameM;
 
 if utvid.settings.nrOrMar ~= 0
+
     subplot(1,3,1), set(gcf, 'Position', get(0,'Screensize'));
     imshow(imnL,[]),  
-    hold on, plot(Xest_or.x1(1,:,n),Xest_or.x1(2,:,n),'r*');
+    hold on, plot(utvid.Tracking.Xest_or.x1(1,:,n),utvid.Tracking.Xest_or.x1(2,:,n),'r*');
 
     subplot(1,3,2), imshow(imnR,[]),
-    hold on, plot(Xest_or.x2(1,:,n),Xest_or.x2(2,:,n),'r*');
+    hold on, plot(utvid.Tracking.Xest_or.x2(1,:,n),utvid.Tracking.Xest_or.x2(2,:,n),'r*');
 
     subplot(1,3,3), imshow(imnM,[]), 
-    hold on, plot(Xest_or.x3(1,:,n),Xest_or.x3(2,:,n),'r*');
+    hold on, plot(utvid.Tracking.Xest_or.x3(1,:,n),utvid.Tracking.Xest_or.x3(2,:,n),'r*');
 
 
 choice = questdlg('Orientation markers located correct', 'User input', ...
@@ -21,19 +22,19 @@ switch choice
         close
     case 'No'
         close
-        Xest_or.x1(:,:,n) = correctPoints(imnL,utvid.settings.nrOrMar,Xest_or.x1(:,:,n),'orientation');
-        Xest_or.x2(:,:,n) = correctPoints(imnR,utvid.settings.nrOrMar,Xest_or.x2(:,:,n),'orientation'); 
-        Xest_or.x3(:,:,n) = correctPoints(imnM,utvid.settings.nrOrMar,Xest_or.x3(:,:,n),'orientation');
+        utvid.Tracking.Xest_or.x1(:,:,n) = correctPoints(imnL,utvid.settings.nrOrMar,utvid.Tracking.Xest_or.x1(:,:,n),'orientation');
+        utvid.Tracking.Xest_or.x2(:,:,n) = correctPoints(imnR,utvid.settings.nrOrMar,utvid.Tracking.Xest_or.x2(:,:,n),'orientation'); 
+        utvid.Tracking.Xest_or.x3(:,:,n) = correctPoints(imnM,utvid.settings.nrOrMar,utvid.Tracking.Xest_or.x3(:,:,n),'orientation');
 
-        Kal_or.meas(:,n) = [Xest_or.x1(1,:,n)';Xest_or.x2(1,:,n)';Xest_or.x3(1,:,n)';Xest_or.x1(2,:,n)';Xest_or.x2(2,:,n)';Xest_or.x3(2,:,n)'];
+        Kal_or.meas(:,n) = [utvid.Tracking.Xest_or.x1(1,:,n)';utvid.Tracking.Xest_or.x2(1,:,n)';utvid.Tracking.Xest_or.x3(1,:,n)';utvid.Tracking.Xest_or.x1(2,:,n)';utvid.Tracking.Xest_or.x2(2,:,n)';utvid.Tracking.Xest_or.x3(2,:,n)'];
         Kal_or      = prepareKalman3D(Kal_or, Pstruct_or, n);
         Kal_or      = updateKal(Kal_or, n);
-        Xest_or     = getSpatialRep(Xest_or, n, Kal_or.Xest(1:end/2,n), Kal_or.Cest(1:end/2,1:end/2,n), Pstruct_or);
+        utvid.Tracking.Xest_or     = getSpatialRep(utvid.Tracking.Xest_or, n, Kal_or.Xest(1:end/2,n), Kal_or.Cest(1:end/2,1:end/2,n), Pstruct_or);
         T = rigid_transform_3D([Kal_or.Xest(1:end/6,n)';Kal_or.Xest(end/6+1:end/6*2,n)';Kal_or.Xest(end/6*2+1:end/6*3,n)']',[Kal_or.Xest(1:end/6,1)';Kal_or.Xest(end/6+1:end/6*2,1)';Kal_or.Xest(end/6*2+1:end/6*3,1)']');
 end
 close
 end
-size(Xest.x1)
+
 h1 = figure; set(gcf, 'Position', get(0,'Screensize'));
 imshow(imnL,[]); hold on
 h2 = plot(Xest.x1(:,1,n),Xest.x1(:,2,n),'*r');
